b17949458b1a495f1e2114940919b2885c2023b7
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-02-26 18:42:16 +0300

    Create base on-messaging

--- PolishBot.Core/Program.cs
+++ PolishBot.Core/Program.cs
@@ -1,10 +1,24 @@
 ﻿using dotenv.net;
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 DotEnv.Load();
-
 var botApiKey = DotEnv.Read()["BOT_API_KEY"];
 
-var bot = new TelegramBotClient(botApiKey);
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient(token: botApiKey, cancellationToken: cts.Token);
 var me = await bot.GetMe();
+
+bot.OnMessage += OnMessage;
+
 Console.WriteLine($"Hello, world! My ID is {me.Id} and my name is {me.FirstName}");
+Console.ReadLine();
+cts.Cancel();
+
+async Task OnMessage(Message message, UpdateType type)
+{
+    if (message.Text is null) return;
+    Console.WriteLine($"Received {type} '{message.Text}' in {message.Chat}");
+    await bot.SendMessage(message.Chat, $"{message.From} said:\n {message.Text}");
+}
