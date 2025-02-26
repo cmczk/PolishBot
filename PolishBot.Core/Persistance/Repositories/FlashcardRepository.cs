10c9f9d59d0b4b009cff045dfb7522b7f21e9008
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-02-26 16:25:17 +0300

    Create repository for flashcards

--- PolishBot.Core/Persistance/Repositories/FlashcardRepository.cs
+++ PolishBot.Core/Persistance/Repositories/FlashcardRepository.cs
@@ -1,6 +1,37 @@
-﻿namespace PolishBot.Core.Persistance.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using PolishBot.Core.Persistance.Models;
 
-public class FlashcardRepository
+namespace PolishBot.Core.Persistance.Repositories;
+
+public class FlashcardRepository(PolishBotDbContext context)
 {
+    private readonly PolishBotDbContext _context = context;
 
+    public async Task AddFlashcard(Flashcard flashcard)
+    {
+        await _context.Flashcards.AddAsync(flashcard);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Flashcard> GetRandomFlashCard()
+    {
+        using var transaction = await _context.Database.BeginTransactionAsync();
+        
+        try
+        {
+            var flashcardsCount = _context.Flashcards.Count();
+            var random = new Random();
+            var randomIndex = random.Next(flashcardsCount);
+
+            await transaction.CommitAsync();
+            
+            return await _context.Flashcards.Skip(randomIndex).FirstOrDefaultAsync()
+                ?? Flashcard.Default;
+        }
+        catch
+        {
+            await transaction.RollbackAsync();
+            return Flashcard.Default;
+        }
+    }
 }
