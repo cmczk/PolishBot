3bb737bf4542cc768be7dd8f09c65ffa2ddd9642
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-02-26 16:03:58 +0300

    Add default Flashcard value

--- PolishBot.Core/Persistance/Models/Flashcard.cs
+++ PolishBot.Core/Persistance/Models/Flashcard.cs
@@ -2,8 +2,23 @@
 
 public class Flashcard
 {
-    public uint Id { get; set; }
+    public int Id { get; set; }
     public string Word { get; set; } = null!;
+    public string Explanation { get; set; } = null!;
     public string Translation { get; set; } = null!;
     public string Example { get; set; } = null!;
+
+    private Flashcard(string word, string explanation, string translation, string example)
+    {
+        Word = word;
+        Explanation = explanation;
+        Translation = translation;
+        Example = example;
+    }
+
+    public static Flashcard Default { get; } = new(
+        "Marzenie ściętej głowy",
+        "Coś nierealnego, niedostępnego dla kogoś", 
+        "Мечта отсечённой головы, нереалистичное желание или устремление",
+        "Chcesz się z nią ożenić? Marzenie ściętej głowy! Ona nigdy się nie zgodzi");
 }
