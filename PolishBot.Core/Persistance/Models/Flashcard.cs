582783284ddc47ff927b7fbaa97e245d8c82fed9
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-02-26 15:17:30 +0300

    Create DbContext and Flashcard model

---
+++ PolishBot.Core/Persistance/Models/Flashcard.cs
@@ -0,0 +1,8 @@
+﻿namespace PolishBot.Core.Persistance.Models;
+
+public class Flashcard
+{
+    public string Word { get; set; } = null!;
+    public string Translation { get; set; } = null!;
+    public string Example { get; set; } = null!;
+}
--- PolishBot.Core/Persistance/PolishBotDbContext.cs
+++ PolishBot.Core/Persistance/PolishBotDbContext.cs
@@ -1,6 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using PolishBot.Core.Persistance.Models;
 
 public class PolishBotDbContext(DbContextOptions<PolishBotDbContext> options) : DbContext(options)
 {
+    DbSet<Flashcard> Flashcards { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=polish_bot.db");
+    }
 }
