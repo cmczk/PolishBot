43acb88f35075dd9004153504d0dabe0d52c0ba2
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-02-26 16:54:12 +0300

    Create minimal bot example

--- PolishBot.Core/Program.cs
+++ PolishBot.Core/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using dotenv.net;
+using Telegram.Bot;
+
+DotEnv.Load();
+
+var botApiKey = DotEnv.Read()["BOT_API_KEY"];
+
+var bot = new TelegramBotClient(botApiKey);
+var me = await bot.GetMe();
+Console.WriteLine($"Hello, world! My ID is {me.Id} and my name is {me.FirstName}");
